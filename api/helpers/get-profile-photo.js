74efb19175d40c640db8e2640ca0fbb7b92f1db0
Adefeyitimi Adeyeloja <33554538+TimmyIsANerd@users.noreply.github.com>
2024-07-05 17:38:32 +0100

    chore: set up helper for getting tg user's profile photo

---
+++ api/helpers/get-profile-photo.js
@@ -0,0 +1,67 @@
+const axios = require('axios')
+
+module.exports = {
+  friendlyName: 'Get profile photo',
+
+  description: '',
+
+  inputs: {
+    userId: {
+      type: 'string',
+      description: 'Telegram Chat ID',
+    },
+  },
+
+  exits: {
+    success: {
+      outputFriendlyName: 'Profile photo',
+    },
+  },
+
+  fn: async function ({ userId }) {
+    const TOKEN = process.env.TELEGRAM_BOT_TOKEN
+    try {
+      // Step 1: Get User Profile Photos
+      const photosResponse = await axios.get(
+        `https://api.telegram.org/bot${TOKEN}/getUserProfilePhotos`,
+        {
+          params: {
+            user_id: userId,
+          },
+        }
+      )
+
+      if (photosResponse.data.result.total_count === 0) {
+        return null
+      }
+      
+      if (
+        photosResponse.data.ok &&
+        photosResponse.data.result.total_count > 0
+      ) {
+        const fileId = photosResponse.data.result.photos[0][0].file_id
+
+        // Step 2: Get File Information
+        const fileResponse = await axios.get(
+          `https://api.telegram.org/bot${TOKEN}/getFile`,
+          {
+            params: {
+              file_id: fileId,
+            },
+          }
+        )
+
+        if (fileResponse.data.ok) {
+          const filePath = fileResponse.data.result.file_path
+
+          // Step 3: Construct the Download URL
+          const fileUrl = `https://api.telegram.org/file/bot${TOKEN}/${filePath}`
+
+          return fileUrl
+        }
+      }
+    } catch (error) {
+      throw new Error('No profile photos found for the user.')
+    }
+  },
+}
