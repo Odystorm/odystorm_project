deaecc8dcec31f245484f240fa206aae6fce4b98
Adefeyitimi Adeyeloja <33554538+TimmyIsANerd@users.noreply.github.com>
2024-07-05 22:38:06 +0100

    chore: realigned elements

--- assets/js/components/BottomNav/index.jsx
+++ assets/js/components/BottomNav/index.jsx
@@ -1,30 +1,22 @@
 import { navLinks } from '@/data/sample'
-import { motion } from 'framer-motion'
 
 const BottomNav = ({ component, setComponent }) => {
   return (
-    <nav className="absolute bottom-5 left-0 right-0 z-10 mx-auto flex w-max items-baseline justify-center gap-x-3 rounded-lg bg-[#AF955D] p-2">
+    <nav className="absolute bottom-0 flex w-full justify-between bg-inherit px-5">
       {navLinks.map((link, index) => {
         const isActive = component === link.component
+        const iconProps = {
+          className: `${isActive ? 'fill-white' : 'fill-[#9E9E9E]'}`,
+        }
         return (
           <div key={index} className="relative flex flex-col items-center">
-            {isActive && (
-              <motion.div
-                className="absolute inset-0 z-0 rounded-lg bg-[#000000]/75"
-                layoutId="activeBackground"
-                initial={{ opacity: 0 }}
-                animate={{ opacity: 1 }}
-                exit={{ opacity: 0 }}
-                transition={{ duration: 0.4, ease: 'easeInOut' }}
-              />
-            )}
             <button
-              className={`relative z-10 flex flex-col items-center px-3 py-2 font-MedievalSharp text-white/50 transition duration-300 ease-in-out ${
-                isActive ? 'font-semibold text-white' : ''
+              className={`relative z-10 flex flex-col items-center px-3 py-2  transition duration-300 ease-in-out focus:outline-none focus:ring-offset-[#AF955D] ${
+                isActive ? 'font-semibold text-white' : 'text-white/50'
               }`}
               onClick={() => setComponent(link.component)}
             >
-              {link.icon}
+              {link.icon(iconProps)}
               <span
                 className={`transition duration-300 ease-in-out ${
                   isActive ? 'text-white' : 'text-white/50'
