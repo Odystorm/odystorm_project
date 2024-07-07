66e79df8a8bbb1249e49320afbbf072a9c1769b9
Adefeyitimi Adeyeloja <33554538+TimmyIsANerd@users.noreply.github.com>
2024-07-07 19:08:19 +0100

    chore: set leaderboard to coming soon

---
+++ assets/js/components/Page/Stats/index.jsx
@@ -0,0 +1,11 @@
+const Stats = () => {
+  return (
+    <div className="flex text-white flex-col justify-center items-center gap-y-3 h-screen w-full">
+      <img src="/images/logo/logo.svg" className="h-[100px] w-[100px] animate-bounce" alt="" />
+      <h3 className="text-4xl font-bold">Leaderboard</h3>
+      <p className="text-lg">Feature currently in development...</p>
+    </div>
+  )
+}
+
+export default Stats
