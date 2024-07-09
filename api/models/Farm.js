f4b56ad8a872f9396647edf25a06d5166a29dedf
Adefeyitimi Adeyeloja <33554538+TimmyIsANerd@users.noreply.github.com>
2024-07-09 10:02:38 +0100

    chore: create model for farming

---
+++ api/models/Farm.js
@@ -0,0 +1,39 @@
+/**
+ * Farm.js
+ *
+ * @description :: A model definition represents a database table/collection.
+ * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
+ */
+
+module.exports = {
+  attributes: {
+    owner: {
+      model: 'activity',
+    },
+    farmSessionId: {
+      type: 'string',
+      description: 'Farm Session ID',
+      example: 'V1StGXR8_Z5jdHi6B-myT',
+    },
+    startTime: {
+      type: 'number',
+      description: 'Start Time in Timestamp format',
+    },
+    endTime: {
+      type: 'number',
+      description: 'End Time in Timestamp format',
+    },
+    hours: {
+      type: 'number',
+      description: 'Number of Farm Hours',
+    },
+    increment: {
+      type: 'number',
+      description: 'Score Level',
+    },
+    savedScore: {
+      type: 'number',
+      description: 'Saved Score',
+    },
+  },
+}
