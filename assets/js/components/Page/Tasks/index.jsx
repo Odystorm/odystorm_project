655778ae0fc29fd320fbc0f2416159ec37b63a55
Adefeyitimi Adeyeloja <33554538+TimmyIsANerd@users.noreply.github.com>
2024-07-10 22:18:27 +0100

    chore: set up maintainance page

--- assets/js/components/Page/Tasks/index.jsx
+++ assets/js/components/Page/Tasks/index.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
-import { tasks } from '@/data/sample'
+// import { tasks } from '@/data/sample'
 
 const Tasks = () => {
   return (
-    <div className="max-h-screen w-full overflow-y-scroll">
+    <div className="relative flex max-h-screen min-h-screen w-full items-center justify-center overflow-y-scroll">
+      {/* <div className='absolute top-0 left-0 h-[100dvh] w-full bg-black'>
+        <h3>Feature is Currently Undergoing Development</h3>
+      </div> */}
       <div className="mt-5 flex flex-col items-center justify-center overflow-y-auto py-3">
         <img
           src="/images/logo/logo.svg"
@@ -12,12 +14,15 @@ const Tasks = () => {
         />
         <div className="text-center">
           <h3 className=" text-3xl font-bold text-white">Tasks</h3>
-          <p className="px-2 text-white/50">
-            You'll be rewarded immediately with OdyStorm Tokens after each task
-            completion.
-          </p>
+          <div className="px-2 text-white/50">
+            <p className="">
+              You'll be rewarded immediately with OdyStorm Tokens after each
+              task completion.
+            </p>
+            <p>Currently undergoing maintenance</p>
+          </div>
         </div>
-        <div className="mb-[5rem] mt-5 w-full space-y-5 px-2">
+        {/* <div className="mb-[5rem] mt-5 w-full space-y-5 px-2">
           {tasks.map((task, index) => {
             const iconProps = { className: 'fill-white h-[40px] w-[40px]' }
             return (
@@ -48,7 +53,7 @@ const Tasks = () => {
               </React.Fragment>
             )
           })}
-        </div>
+        </div> */}
       </div>
     </div>
   )
