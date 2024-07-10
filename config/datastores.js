a7e3e17c4e0d2a420179945818af4fbe00651a95
Adefeyitimi Adeyeloja <33554538+TimmyIsANerd@users.noreply.github.com>
2024-07-10 16:06:51 +0100

    switch back to mongodb

--- config/datastores.js
+++ config/datastores.js
@@ -46,6 +46,6 @@ module.exports.datastores = {
      *                                                                          *
      ***************************************************************************/
     adapter: 'sails-mongo',
-    url: process.env.MONGODB_LOCAL,
+    url: 'mongodb+srv://adefeyitimi:Uk2ASlJJueClKWOP@odystorm.9j0y38p.mongodb.net',
   },
 }
