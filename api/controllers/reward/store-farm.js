cec36909b967f9b53e75105599921c48b8bca119
Adefeyitimi Adeyeloja <33554538+TimmyIsANerd@users.noreply.github.com>
2024-07-11 02:26:20 +0100

    refactor: reworked farm store logic

--- api/controllers/reward/store-farm.js
+++ api/controllers/reward/store-farm.js
@@ -9,16 +9,17 @@ module.exports = {
       description: "Current User's Telegram Id",
       required: true,
     },
-    timeline: {
-      type: 'json',
-      description: 'Start and End Time Data',
-      required: true,
+    startTime: {
+      type: 'number',
+      description: 'Timestamp of Start Time',
     },
-    farmSessionId: {
-      type: 'string',
-      description: 'Farm Session ID',
-      example: 'V1StGXR8_Z5jdHi6B-myT',
-      required: true,
+    endTime: {
+      type: 'number',
+      description: 'Timestamp of End Time',
+    },
+    hours: {
+      type: 'number',
+      description: 'No. Of Farming hours',
     },
     eligibleClaimAmount: {
       type: 'number',
@@ -28,7 +29,13 @@ module.exports = {
 
   exits: {},
 
-  fn: async function ({ telegramId, timeline, farmSessionId }) {
+  fn: async function ({
+    telegramId,
+    startTime,
+    endTime,
+    hours,
+    eligibleClaimAmount,
+  }) {
     const { res } = this
 
     try {
@@ -37,14 +44,13 @@ module.exports = {
       const activity = await Activity.findOne({ owner: userRecord.id })
 
       // Create New Farm Record
-      await Farm.create({
+      const newFarm = await Farm.create({
         activity: activity.id,
-        farmSessionId,
-        startTime: timeline.startTime,
-        endTime: timeline.endTime,
-        hours: timeline.hours,
-        increment: timeline.increment,
-        eligibleClaimAmount: timeline.savedScore ? timeline.savedScore : 0,
+        startTime: startTime,
+        endTime: endTime,
+        hours: hours,
+        increment: activity.farmLevel,
+        eligibleClaimAmount: eligibleClaimAmount,
       }).fetch()
 
       await Activity.updateOne({ id: activity.id }).set({
@@ -53,6 +59,7 @@ module.exports = {
 
       return res.status(200).json({
         message: 'Successfully updated timeline',
+        ...newFarm,
       })
     } catch (error) {
       sails.log.error(error)
