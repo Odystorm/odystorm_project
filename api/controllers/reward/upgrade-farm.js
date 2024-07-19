82723f07a283874e1de5ee22b0762edc8568ff79
Adefeyitimi Adeyeloja <33554538+TimmyIsANerd@users.noreply.github.com>
2024-07-19 19:44:21 +0100

    feat: implement upgrading user

---
+++ api/controllers/reward/upgrade-farm.js
@@ -0,0 +1,50 @@
+module.exports = {
+  friendlyName: 'reward upgrade-farm',
+
+  description: '',
+
+  inputs: {
+    telegramId: {
+      type: 'string',
+      description: "Current User's Telegram Id",
+    },
+    upgrade: {
+      type: 'json',
+      description: 'Selected Upgrade Object',
+    },
+  },
+
+  exits: {},
+
+  fn: async function ({ telegramId, upgrade }) {
+    const { res } = this
+
+    try {
+      // Find User
+      const userRecord = await User.findOne({ chatId: telegramId })
+      const wallet = await Wallet.findOne({ owner: userRecord.id })
+
+      // Verify User Can Afford Upgrade
+      if (upgrade.cost > wallet.balance) {
+        return res.badRequest({ message: "You can't afford this upgrade" })
+      }
+
+      await Activity.updateOne({
+        owner: userRecord.id,
+      }).set({
+        farmLevel: upgrade.Increment,
+        currentNoOfFarmHours: upgrade.FarmPeriod,
+      })
+
+      return res.status(200).json({
+        message: 'Successfully upgraded Farm Power',
+      })
+    } catch (error) {
+      sails.log.error(error)
+
+      return res.serverError({
+        message: 'There was a problem upgrading your space ship',
+      })
+    }
+  },
+}
