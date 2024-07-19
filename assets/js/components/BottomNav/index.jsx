c7d0d56c85a79e014dec7d08424446aa23d21c38
Adefeyitimi Adeyeloja <33554538+TimmyIsANerd@users.noreply.github.com>
2024-07-19 16:27:38 +0100

    chore: restyled theme

--- assets/js/components/BottomNav/index.jsx
+++ assets/js/components/BottomNav/index.jsx
@@ -3,17 +3,20 @@ import { navLinks } from '@/data/sample'
 
 const BottomNav = ({ component, setComponent }) => {
   return (
-    <nav className="absolute bottom-0 flex w-full justify-between bg-white px-5 py-3">
+    <nav className="absolute bottom-0 flex w-full justify-between bg-black bg-opacity-20 px-5 py-3 shadow-lg backdrop-blur-md backdrop-filter">
       {navLinks.map((link, index) => {
         const isActive = component === link.component
         const iconProps = {
-          className: `${isActive ? 'fill-white' : 'fill-black/80'}`,
+          className: `${isActive ? 'fill-white' : 'fill-white/80'}`,
         }
         return (
-          <div key={index} className="relative flex flex-col items-center">
+          <div
+            key={index}
+            className="relative flex flex-col items-center bg-opacity-100"
+          >
             {isActive && (
               <motion.div
-                className="absolute inset-0 z-0 rounded-lg bg-gradient-to-r from-cyan-400 to-blue-500"
+                className="absolute inset-0 z-0 rounded-lg bg-gradient-to-r from-cyan-400 to-blue-500  shadow-blue-500 shadow-2xl"
                 layoutId="activeBackground"
                 initial={{ opacity: 0 }}
                 animate={{ opacity: 1 }}
@@ -23,14 +26,14 @@ const BottomNav = ({ component, setComponent }) => {
             )}
             <button
               className={`relative z-10 flex flex-col items-center px-3 py-2  transition duration-300 ease-in-out ${
-                isActive ? 'font-semibold text-black' : 'text-black/50'
+                isActive ? 'font-semibold text-black' : 'text-white/50'
               }`}
               onClick={() => setComponent(link.component)}
             >
               {link.icon(iconProps)}
               <span
                 className={`transition duration-500 ease-in-out ${
-                  isActive ? 'text-white' : 'text-black/50'
+                  isActive ? 'text-white' : 'text-white/50'
                 }`}
               >
                 {link.title}
