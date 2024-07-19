cf2c03edbc96e14c827f9cc1ab1892ad42b525e8
Adefeyitimi Adeyeloja <33554538+TimmyIsANerd@users.noreply.github.com>
2024-07-19 19:53:38 +0100

    feat: implement getting tasks

---
+++ api/controllers/user/get-tasks.js
@@ -0,0 +1,26 @@
+module.exports = {
+  friendlyName: 'Get tasks',
+
+  description: '',
+
+  inputs: {},
+
+  exits: {},
+
+  fn: async function (inputs) {
+    const { req, res } = this
+    const { tgId } = req.params
+
+    try {
+      const userRecord = await User.findOne({ chatId: tgId })
+      const tasks = await Tasks.find({ owner: userRecord.id })
+
+      return res.status(200).json(tasks)
+    } catch (error) {
+      sails.log.error(error)
+      return res.serverError({
+        message: 'Failed to retrieve tasks data',
+      })
+    }
+  },
+}
