030cb00df8d0b6fc02a3a857d44a7751a71d3ba6
Adefeyitimi Adeyeloja <33554538+TimmyIsANerd@users.noreply.github.com>
2024-07-20 18:37:45 +0100

    feat: update social task record

---
+++ api/controllers/reward/click-social-task.js
@@ -0,0 +1,62 @@
+module.exports = {
+  friendlyName: 'Click social task',
+
+  description: '',
+
+  inputs: {
+    telegramId: {
+      type: 'string',
+      description: "Current User's Telegram Id",
+    },
+    claimedTask: {
+      type: 'json',
+      description: 'Claimed Task',
+    },
+  },
+
+  exits: {},
+
+  fn: async function ({ telegramId, claimedTask }) {
+    const { res } = this
+
+    try {
+      const userRecord = await User.findOne({ chatId: telegramId })
+      const task = await Tasks.findOne({
+        id: claimedTask.id,
+        owner: userRecord.id,
+        taskType: 'social_following',
+      })
+
+      if (!task) {
+        return res.notFound({
+          message: 'Task not found',
+        })
+      }
+
+      if (task.status === 'done') {
+        return res.badRequest({
+          message: 'Task is already done',
+        })
+      }
+
+      await Tasks.updateOne({
+        id: task.id,
+      }).set({
+        requirement: {
+          ...task.requirement,
+          isClicked: true,
+        },
+      })
+
+      return res.status(200).json({
+        message: 'Successfully clicked task',
+      })
+    } catch (error) {
+      console.error(error)
+
+      return res.serverError({
+        message: 'Failed to claim task reward',
+      })
+    }
+  },
+}
