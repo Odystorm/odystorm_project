c169155dff2fafc360970ee7e862a4df537c6cc5
Adefeyitimi Adeyeloja <33554538+TimmyIsANerd@users.noreply.github.com>
2024-07-20 18:38:32 +0100

    chore: task generator

--- api/helpers/generate-bot-tasks.js
+++ api/helpers/generate-bot-tasks.js
@@ -1,81 +1,136 @@
 const tasks = [
-  // {
-  //   title: 'Join OdyStorm Telegram',
-  //   rewardAmount: 10000,
-  //   taskType: 'social_following',
-  //   requirement: {
-  //     url:''
-  //   },
-  //   icon: '$ody_logo',
-  // },
+  {
+    title: 'Join OdyStorm Telegram',
+    rewardAmount: 10000,
+    taskType: 'social_following',
+    requirement: {
+      mineTotal: 0,
+      url: 'https://t.me/odystormofficial',
+      isClicked: false,
+    },
+    icon: 'telegram',
+  },
+  {
+    title: 'Follow OdyStorm on X',
+    rewardAmount: 10000,
+    taskType: 'social_following',
+    requirement: {
+      mineTotal: 0,
+      url: 'https://x.com/odystorm',
+      isClicked: false,
+    },
+    icon: 'x',
+  },
+  {
+    title: 'Subscribe To OdyStorm YouTube',
+    rewardAmount: 10000,
+    taskType: 'social_following',
+    requirement: {
+      mineTotal: 0,
+      url: 'https://www.youtube.com/@odystormofficial',
+      isClicked: false,
+    },
+    icon: 'youtube',
+  },
+  {
+    title: 'OdyStorm on Tik Tok',
+    rewardAmount: 10000,
+    taskType: 'social_following',
+    requirement: {
+      mineTotal: 0,
+      url: 'https://www.tiktok.com/@odystormofficial',
+      isClicked: false,
+    },
+    icon: 'youtube',
+  },
   {
     title: 'Mine 50,000 $ODY',
     rewardAmount: 10000,
     taskType: 'milestone',
-    requirement: 50000,
+    requirement: {
+      mineTotal: 50000,
+    },
     icon: '$ody_logo',
   },
   {
     title: 'Mine 100,000 $ODY',
     rewardAmount: 20000,
     taskType: 'milestone',
-    requirement: 100000,
+    requirement: {
+      mineTotal: 100000,
+    },
     icon: '$ody_logo',
   },
   {
     title: 'Mine 200,000 $ODY',
     rewardAmount: 40000,
     taskType: 'milestone',
-    requirement: 200000,
+    requirement: {
+      mineTotal: 200000,
+    },
     icon: '$ody_logo',
   },
   {
     title: 'Mine 400,000 $ODY',
     rewardAmount: 80000,
     taskType: 'milestone',
-    requirement: 400000,
+    requirement: {
+      mineTotal: 400000,
+    },
     icon: '$ody_logo',
   },
   {
     title: 'Mine 800,000 $ODY',
     rewardAmount: 160000,
     taskType: 'milestone',
-    requirement: 800000,
+    requirement: {
+      mineTotal: 800000,
+    },
     icon: '$ody_logo',
   },
   {
     title: 'Mine 1,600,000 $ODY',
     rewardAmount: 320000,
     taskType: 'milestone',
-    requirement: 1600000,
+    requirement: {
+      mineTotal: 1600000,
+    },
     icon: '$ody_logo',
   },
   {
     title: 'Mine 3,200,000 $ODY',
     rewardAmount: 640000,
     taskType: 'milestone',
-    requirement: 3200000,
+    requirement: {
+      mineTotal: 3200000,
+    },
     icon: '$ody_logo',
   },
   {
     title: 'Mine 6,400,000 $ODY',
     rewardAmount: 1280000,
     taskType: 'milestone',
-    requirement: 6400000,
+    requirement: {
+      mineTotal: 6400000,
+    },
     icon: '$ody_logo',
   },
   {
     title: 'Mine 12,800,000 $ODY',
     rewardAmount: 2560000,
     taskType: 'milestone',
-    requirement: 12800000,
+    requirement: {
+      mineTotal: 12800000,
+    },
     icon: '$ody_logo',
   },
   {
     title: 'Mine 25,600,000 $ODY',
     rewardAmount: 5120000,
     taskType: 'milestone',
-    requirement: 25600000,
+    requirement: {
+      mineTotal: 25600000,
+    },
     icon: '$ody_logo',
   },
 ]
@@ -108,10 +163,19 @@ module.exports = {
             title: task.title,
             rewardAmount: task.rewardAmount,
             taskType: task.taskType,
-            requirement: {
-              mineTotal: task.requirement,
-            },
+            requirement: task.requirement,
             icon: 'ody_logo',
+          })
+        }
+
+        if (task.taskType === 'social_following') {
+          await Tasks.create({
+            owner: id,
+            title: task.title,
+            rewardAmount: task.rewardAmount,
+            taskType: task.taskType,
+            requirement: task.requirement,
+            icon: task.icon,
           })
         }
       } catch (error) {
