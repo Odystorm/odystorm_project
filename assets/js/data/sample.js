e84c65e05c91b0b32181731c5d0d6cae023dcc82
Adefeyitimi Adeyeloja <33554538+TimmyIsANerd@users.noreply.github.com>
2024-07-22 00:24:24 +0100

    chore: changed bottom nav

--- assets/js/data/sample.js
+++ assets/js/data/sample.js
@@ -45,16 +45,18 @@ export const navLinks = [
     component: 'referral',
   },
   {
-    title: 'Rank',
+    title: 'Stats',
     icon: (props) => (
       <svg
         xmlns="http://www.w3.org/2000/svg"
         width="32"
         height="32"
-        viewBox="0 0 24 24"
+        viewBox="0 0 512 512"
         {...props}
       >
-        <path d="M7 21v-2h4v-3.1q-1.225-.275-2.187-1.037T7.4 12.95q-1.875-.225-3.137-1.637T3 8V7q0-.825.588-1.412T5 5h2V3h10v2h2q.825 0 1.413.588T21 7v1q0 1.9-1.263 3.313T16.6 12.95q-.45 1.15-1.412 1.913T13 15.9V19h4v2zm0-10.2V7H5v1q0 .95.55 1.713T7 10.8m10 0q.9-.325 1.45-1.088T19 8V7h-2z" />
+        <path
+          d="M128 496H48V304h80Zm224 0h-80V208h80Zm112 0h-80V96h80Zm-224 0h-80V16h80Z"
+        />
       </svg>
     ),
     component: 'stats',
