f86f81a495a177638086fca9248fa365a7d99233
Adefeyitimi Adeyeloja <33554538+TimmyIsANerd@users.noreply.github.com>
2024-07-22 15:08:45 +0100

    chore: deployed cron job for every 5 minutes and every 12 hours

--- config/cronjob.js
+++ config/cronjob.js
@@ -1,12 +1,15 @@
 const axios = require('axios')
 
+const liveUrl = `https://odystorm-bot.onrender.com`
+const baseURL = liveUrl
+
 module.exports.cronjob = {
-  sendReminderEvery3Hours: {
+  checkEvery5Minutes: {
     on: 'ready',
-    schedule: '0 */3 * * *', // Run every 3 hours
+    schedule: '*/5 * * * *', // Run every 5 Minutes
     onTick: async function () {
-      sails.log.info('Currently Running System Checks...')
-      const url = `https://odystorm-bot.onrender.com/mine/session/reminder`
+      sails.log.info('Currently Running 10 Minute After Mine Reminder...')
+      const url = `${baseURL}/mine/session/reminder/recent`
       try {
         const response = await axios.get(url)
         sails.log.info(response.status)
@@ -14,5 +17,19 @@ module.exports.cronjob = {
         sails.log.error('Failed to Run Reminder Cron Job')
       }
     },
+  },
+  checkEveryHour: {
+    schedule: '0 * * * *', // This cron expression means "at minute 0 of every hour"
+    onTick: async function () {
+      sails.log.info('Currently Running 12 Hours After Mine Reminder...')
+      const url = `${baseURL}/mine/session/reminder/hours`
+      try {
+        const response = await axios.get(url)
+        sails.log.info(response.status)
+      } catch (error) {
+        sails.log.error('Failed to Run Reminder Cron Job')
+      }
+    },
+    start: true,
   },
 }
