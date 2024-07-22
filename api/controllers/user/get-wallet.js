b6d78e8534682157cdc6225ca311b5743b850a27
Adefeyitimi Adeyeloja <33554538+TimmyIsANerd@users.noreply.github.com>
2024-07-22 21:16:33 +0100

    chore: modifed getting wallet action

--- api/controllers/user/get-wallet.js
+++ api/controllers/user/get-wallet.js
@@ -14,8 +14,9 @@ module.exports = {
     try {
       const userRecord = await User.findOne({ chatId: tgId })
       const wallet = await Wallet.findOne({ owner: userRecord.id })
+      const activity = await Activity.findOne({ owner: userRecord.id })
 
-      return res.status(200).json(wallet)
+      return res.status(200).json({ ...wallet, activity })
     } catch (error) {
       return res.serverError({
         message: 'Failed to retrieve wallet data',
