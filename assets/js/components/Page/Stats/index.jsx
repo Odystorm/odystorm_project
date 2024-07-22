5b511ae9dae60635a56cc1abbf4f111332b82069
Adefeyitimi Adeyeloja <33554538+TimmyIsANerd@users.noreply.github.com>
2024-07-22 01:25:57 +0100

    feat: implemented stats viewer

--- assets/js/components/Page/Stats/index.jsx
+++ assets/js/components/Page/Stats/index.jsx
@@ -1,9 +1,75 @@
+import axios from 'axios'
+import { useEffect, useState } from 'react'
+import { Puff } from 'react-loader-spinner'
+
 const Stats = () => {
+  const [loading, setLoading] = useState(true)
+  const [stats, setStats] = useState(null)
+
+  async function getStats() {
+    try {
+      const response = await axios.get('/api/v1/stats')
+      setStats(response.data)
+      setLoading(false)
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  useEffect(() => {
+    getStats()
+  }, [])
+
   return (
-    <div className="flex text-white flex-col justify-center items-center gap-y-3 h-screen w-full">
-      <img src="/images/logo/logo.svg" className="h-[100px] w-[100px] animate-bounce" alt="" />
-      <h3 className="text-4xl font-bold">Leaderboard</h3>
-      <p className="text-lg">Feature currently in development...</p>
+    <div className="relative flex max-h-screen min-h-screen w-full flex-col items-center justify-start gap-y-3 p-5 text-white">
+      <img
+        src="/images/logo/logo.svg"
+        className="animate h-[100px] w-[100px]"
+        alt=""
+      />
+      <h3 className="bg-gradient-to-r from-cyan-400 to-blue-500 bg-clip-text font-orbitron text-4xl font-bold text-transparent shadow-blue-500 drop-shadow-2xl">
+        Stats
+      </h3>
+      {!loading ? (
+        <>
+          <div className="flex flex-col items-center justify-center text-center">
+            <p className="text-md font-orbitron font-extrabold text-white">
+              Total $ODY Mined
+            </p>
+            <span className="text2xl font-orbitron">
+              <span className="font-semibold">$ODY</span>{' '}
+              {stats.totalMined.toLocaleString()}
+            </span>
+          </div>
+          <div className="w-full border border-white"></div>
+          <div className="flex flex-col items-center justify-center text-center">
+            <p className="text-md font-orbitron font-extrabold text-white">
+              No. of Mine Sessions
+            </p>
+            <span className="font-orbitron text-xl">
+              {stats.noOfMineSessions.toLocaleString()}
+            </span>
+          </div>
+          <div className="flex flex-col items-center justify-center text-center">
+            <p className="text-md font-orbitron font-extrabold text-white">
+              OdyStorm Army Total
+            </p>
+            <span className="font-orbitron text-xl">
+              {stats.armyTotal.toLocaleString()}
+            </span>
+          </div>
+          <div className="flex flex-col items-center justify-center text-center">
+            <p className="text-md font-orbitron font-extrabold text-white">
+              Soldiers Reported Today
+            </p>
+            <span className="font-orbitron text-xl">
+              {stats.reportedToday.toLocaleString()}
+            </span>
+          </div>
+        </>
+      ) : (
+        <Puff color="#fff" height={55} width={55} />
+      )}
     </div>
   )
 }
