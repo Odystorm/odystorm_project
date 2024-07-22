5aa827fe04d8c4e6b448064aacdec047ab621702
Adefeyitimi Adeyeloja <33554538+TimmyIsANerd@users.noreply.github.com>
2024-07-22 02:00:52 +0100

    chore: changed daily bonus notification to toast

--- assets/js/pages/play/index.jsx
+++ assets/js/pages/play/index.jsx
@@ -5,7 +5,7 @@ import Stats from '@/components/Page/Stats'
 import Tasks from '@/components/Page/Tasks'
 import Farm from '@/components/Page/Farm'
 import Referrals from '@/components/Page/Referrals'
-import { ToastContainer } from 'react-toastify'
+import { toast, ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
 import { motion, AnimatePresence } from 'framer-motion'
 import { router } from '@inertiajs/react'
@@ -13,7 +13,7 @@ import { router } from '@inertiajs/react'
 export function Notification({ activity, setShowNotification }) {
   return (
     <motion.div
-      className="absolute left-0 top-0 z-[100] flex min-h-screen w-full flex-col items-center justify-center gap-y-3 bg-black text-center text-white font-orbitron"
+      className="absolute left-0 top-0 z-[100] flex min-h-screen w-full flex-col items-center justify-center gap-y-3 bg-black text-center font-orbitron text-white"
       onClick={() => {
         setShowNotification(false)
         window.location.reload()
@@ -42,6 +42,15 @@ const Play = ({ user }) => {
     router.get('/')
   }
 
+  useEffect(() => {
+    if (user.activity.eligibleDailyBonus) {
+      toast.success(
+        `Day ${user.activity.noOfActiveDays} You have Received your Daily Bonus of $ODY 100`
+      )
+      window.location.reload()
+    }
+  }, [])
+
   function renderComponent() {
     switch (component) {
       case 'home':
@@ -65,14 +74,6 @@ const Play = ({ user }) => {
         toastStyle={{ backgroundColor: 'white', color: 'black' }}
         hideProgressBar={true}
       />
-      <AnimatePresence initial={false} mode="sync" exitBeforeEnter={true}>
-        {showNotification && user.activity.eligibleDailyBonus && (
-          <Notification
-            activity={user.activity}
-            setShowNotification={setShowNotification}
-          />
-        )}
-      </AnimatePresence>
     </GameLayout>
   )
 }
