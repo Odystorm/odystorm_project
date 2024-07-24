9ea397af68a8d8fde93e7dfc2c641005bdb05ec8
Adefeyitimi Adeyeloja <33554538+TimmyIsANerd@users.noreply.github.com>
2024-07-24 20:09:25 +0100

    fix: resize layout

--- assets/js/layouts/ingame.jsx
+++ assets/js/layouts/ingame.jsx
@@ -1,20 +1,12 @@
 import { Head } from '@inertiajs/react'
-// import TopNav from '@/components/TopNav'
 import BottomNav from '@/components/BottomNav'
 
 export const GameLayout = ({ children, component, setComponent }) => {
   return (
     <>
       <Head title="Play OdyStorm Token Farm" />
-      <section className="relative max-h-screen min-h-screen w-full overflow-hidden bg-black">
-        {/* <TopNav 
-        /> */}
-        <img
-          src="/images/bg/bg_space.webp"
-          alt=""
-          className="absolute left-0 top-0 h-screen object-cover brightness-[0.45]"
-        />
-        <main className="z-50 min-w-full">{children}</main>
+      <section className="relative max-h-[100dvh] min-h-[100dvh] w-full overflow-hidden flex flex-col items-center justify-between bg-space">
+        <main className="min-w-full max-h-fit border">{children}</main>
         <BottomNav component={component} setComponent={setComponent} />
       </section>
     </>
