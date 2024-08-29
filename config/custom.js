5c432ae879398cadee6344f0c41d3e1d7006f841
Adefeyitimi Adeyeloja <33554538+TimmyIsANerd@users.noreply.github.com>
2024-08-29 08:58:44 +0100

    feat: set new live link

--- config/custom.js
+++ config/custom.js
@@ -17,5 +17,5 @@ module.exports.custom = {
   // sendgridSecret: 'SG.fake.3e0Bn0qSQVnwb1E4qNPz9JZP5vLZYqjh7sn8S93oSHU',
   // stripeSecret: 'sk_test_Zzd814nldl91104qor5911gjald',
   // …
-  botBaseURL: 'https://odystorm-bot.onrender.com',
+  botBaseURL: 'https://odystorm-project.onrender.com/',
 }
